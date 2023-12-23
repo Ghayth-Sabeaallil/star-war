05e0f799e9d62c17b8279d079831aeb607d1e0d6
Ghayth Sabeaallil <ghaessesa58@gmail.com>
2023-12-23 16:54:09 +0100

    calling all html element from the first box

---
+++ src/script.ts
@@ -0,0 +1,8 @@
+/* Selectors */
+const listBox = document.querySelector(".list-characters") as HTMLElement;
+const leftBtn = document.querySelector(".left-btn") as HTMLSpanElement;
+const rightBtn = document.querySelector(".right-btn") as HTMLSpanElement;
+const currentPage = document.querySelector(".current-page") as HTMLSpanElement;
+const numberOfPage = document.querySelector(
+  ".number-of-pages"
+) as HTMLSpanElement;
