2c4327e52cc66f2cf961261b875450f22fe3a290
Ghayth Sabeaallil <ghaessesa58@gmail.com>
2023-12-24 19:52:15 +0100

    fetch data for the first page and set 10 items in the list

---
+++ dist/script/script.js
@@ -0,0 +1,56 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+/* Selectors */
+const listItems = document.querySelectorAll(".item");
+const listCharacters = document.querySelector(".list-characters");
+const leftBtn = document.querySelector(".left-btn");
+const rightBtn = document.querySelector(".right-btn");
+const currentPage = document.querySelector(".current-page");
+const numberOfPage = document.querySelector(".number-of-pages");
+const loadingCharacters = document.querySelector(".loading");
+/* URLS */
+const url = "https://swapi.dev/api/people/?page=1";
+/* Variable */
+let itemsStyle = 0;
+function fetchData() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield fetch(url);
+            if (response.status === 200) {
+                const data = yield response.json();
+                const character = data.results;
+                character.forEach((element) => {
+                    itemsStyle++;
+                    const div = document.createElement("div");
+                    if (itemsStyle == 1 || itemsStyle == 5 || itemsStyle == 9)
+                        div.setAttribute("class", "item test1");
+                    if (itemsStyle == 2 ||
+                        itemsStyle == 4 ||
+                        itemsStyle == 6 ||
+                        itemsStyle == 8 ||
+                        itemsStyle == 10)
+                        div.setAttribute("class", "item test2");
+                    if (itemsStyle == 3 || itemsStyle == 7)
+                        div.setAttribute("class", "item test3");
+                    div.innerHTML = element.name;
+                    listCharacters.append(div);
+                });
+                loadingCharacters.style.display = "none";
+            }
+            else {
+                throw Error("Något gick fel, försök igen senare");
+            }
+        }
+        catch (error) {
+            console.log(error);
+        }
+    });
+}
+fetchData();
--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,8 @@
 /* Selectors */
-const listBox = document.querySelector(".list-characters") as HTMLElement;
+const listItems = document.querySelectorAll(".item") as NodeListOf<Element>;
+const listCharacters = document.querySelector(
+  ".list-characters"
+) as HTMLSpanElement;
 const leftBtn = document.querySelector(".left-btn") as HTMLSpanElement;
 const rightBtn = document.querySelector(".right-btn") as HTMLSpanElement;
 const currentPage = document.querySelector(".current-page") as HTMLSpanElement;
@@ -9,3 +12,70 @@ const numberOfPage = document.querySelector(
 const loadingCharacters = document.querySelector(
   ".loading"
 ) as HTMLImageElement;
+
+/* URLS */
+const url = "https://swapi.dev/api/people/?page=1";
+
+/* Variable */
+let itemsStyle: number = 0;
+
+/* Interfaces */
+interface starWar {
+  count: number;
+  next: string;
+  previous: string;
+  results: characters;
+}
+
+interface characters {
+  forEach: any;
+  name: string;
+  height: number;
+  mass: number;
+  hair_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+  homeworld: string;
+  films: string[];
+  species: string[];
+  vehicles: string[];
+  starships: string[];
+  created: string;
+  edited: string;
+  url: string;
+}
+
+async function fetchData(): Promise<void> {
+  try {
+    const response = await fetch(url);
+    if (response.status === 200) {
+      const data: starWar = await response.json();
+      const character: characters = data.results;
+      character.forEach((element) => {
+        itemsStyle++;
+        const div = document.createElement("div") as HTMLElement;
+        if (itemsStyle == 1 || itemsStyle == 5 || itemsStyle == 9)
+          div.setAttribute("class", "item test1");
+        if (
+          itemsStyle == 2 ||
+          itemsStyle == 4 ||
+          itemsStyle == 6 ||
+          itemsStyle == 8 ||
+          itemsStyle == 10
+        )
+          div.setAttribute("class", "item test2");
+        if (itemsStyle == 3 || itemsStyle == 7)
+          div.setAttribute("class", "item test3");
+        div.innerHTML = element.name;
+        listCharacters.append(div);
+      });
+      loadingCharacters.style.display = "none";
+    } else {
+      throw Error("Något gick fel, försök igen senare");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+fetchData();
